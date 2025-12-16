f2c6eac17d2bbf2c23222402524bbc789ba94be6
mishra-aashu <aashutoshk625@gmail.com>
2025-12-16 13:42:42 +0530

    gg

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  // If we are building for production (GitHub Pages), use the repo name
-  if (command !== 'serve') {
-    config.base = '/caba-android-app/'
-  }
-
-  return config
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+  ],
 })
