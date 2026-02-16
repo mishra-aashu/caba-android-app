4921553288acc50442317bde76c9678fd0cd39af
Aashu <aashutoshk625@gmail.com>
2026-02-16 20:25:56 +0530

    done

--- src/components/auth/ResetPassword.jsx
+++ src/components/auth/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSupabase } from '../../contexts/SupabaseContext';
 import { useNavigate, Link } from 'react-router-dom';
-import '../../styles/auth.css';
+import '../../styles/LoginPage.css';
 
 const ResetPassword = () => {
   const { supabase } = useSupabase();
