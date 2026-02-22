544389a40d062b618ba88f1869f44bc6392adea0
mishra-aashu <mishra-aashu@users.noreply.github.com>
2026-02-22 18:34:13 +0000

    deploy: 171266a287389aeed02e7f110d2f755f0377783e

---
+++ assets/module-JYHuDPlv.js
@@ -0,0 +1,1 @@
+import{r,ap as a,a4 as c}from"./index-C58utrsd.js";function f(t){const n=r.useRef(null),e=r.useRef(null);return e.current&&e.current.update(t),r.useEffect(()=>(e.current=new a({...t,ref:n}),()=>{e.current=null}),[]),c.createElement("div",{ref:n})}export{f as default};
