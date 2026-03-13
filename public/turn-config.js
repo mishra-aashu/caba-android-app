ef2ddc93406258c83b8e7b8ac21b1e82c3998b22
Aashu <aashutoshk625@gmail.com>
2026-03-14 01:25:03 +0530

    feat: Update WebRTC ICE server configurations by adding Cloudflare STUN, consolidating TURN URLs into arrays, and refreshing additional STUN server lists.

--- public/turn-config.js
+++ public/turn-config.js
@@ -12,47 +12,42 @@ const FREE_TURN_SERVERS = {
         { urls: 'stun:stun3.l.google.com:19302' },
         { urls: 'stun:stun4.l.google.com:19302' },
 
-        // OpenRelay TURN (FREE - no signup required)
+        // Cloudflare STUN (NEW - very fast)
+        { urls: 'stun:stun.cloudflare.com:3478' },
+
+        // OpenRelay TURN (Best Free Option)
         {
-            urls: 'turn:openrelay.metered.ca:80',
-            username: 'openrelayproject',
-            credential: 'openrelayproject'
-        },
-        {
-            urls: 'turn:openrelay.metered.ca:443',
-            username: 'openrelayproject',
-            credential: 'openrelayproject'
-        },
-        {
-            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            urls: [
+                'turn:openrelay.metered.ca:80',
+                'turn:openrelay.metered.ca:443',
+                'turn:openrelay.metered.ca:443?transport=tcp',
+                'turns:openrelay.metered.ca:443'
+            ],
             username: 'openrelayproject',
             credential: 'openrelayproject'
         },
 
         // Metered.ca Free TURN (limited but reliable)
         {
-            urls: 'turn:a.relay.metered.ca:80',
-            username: 'df4e050fc5de5dc26b25b85a',
-            credential: 'Pxdp2PK0b5ZXljOm'
-        },
-        {
-            urls: 'turn:a.relay.metered.ca:80?transport=tcp',
-            username: 'df4e050fc5de5dc26b25b85a',
-            credential: 'Pxdp2PK0b5ZXljOm'
-        },
-        {
-            urls: 'turn:a.relay.metered.ca:443',
-            username: 'df4e050fc5de5dc26b25b85a',
-            credential: 'Pxdp2PK0b5ZXljOm'
-        },
-        {
-            urls: 'turn:a.relay.metered.ca:443?transport=tcp',
+            urls: [
+                'turn:a.relay.metered.ca:80',
+                'turn:a.relay.metered.ca:80?transport=tcp',
+                'turn:a.relay.metered.ca:443',
+                'turns:a.relay.metered.ca:443?transport=tcp'
+            ],
             username: 'df4e050fc5de5dc26b25b85a',
             credential: 'Pxdp2PK0b5ZXljOm'
         },
 
-        // Twilio STUN (public)
-        { urls: 'stun:global.stun.twilio.com:3478' }
+        // Twilio STUN (very reliable)
+        { urls: 'stun:global.stun.twilio.com:3478' },
+
+        // Additional Reliable STUN Servers
+        { urls: 'stun:stun.nextcloud.com:443' },
+        { urls: 'stun:stun.services.mozilla.com:3478' },
+        { urls: 'stun:stun.sipgate.net:3478' },
+        { urls: 'stun:stun.1und1.de:3478' },
+        { urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443' }
     ],
     iceCandidatePoolSize: 10
 };
--- src/constants/webrtcConfig.js
+++ src/constants/webrtcConfig.js
@@ -9,49 +9,40 @@ export const ICE_SERVERS = [
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
 
-    // OpenRelay TURN (FREE - no signup required)
+    // Cloudflare STUN (NEW - very fast)
+    { urls: 'stun:stun.cloudflare.com:3478' },
+
+    // OpenRelay TURN (Best Free Option)
     {
-        urls: 'turn:openrelay.metered.ca:80',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-    },
-    {
-        urls: 'turn:openrelay.metered.ca:443',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-    },
-    {
-        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+        urls: [
+            'turn:openrelay.metered.ca:80',
+            'turn:openrelay.metered.ca:443',
+            'turn:openrelay.metered.ca:443?transport=tcp',
+            'turns:openrelay.metered.ca:443'
+        ],
         username: 'openrelayproject',
         credential: 'openrelayproject'
     },
 
     // Metered.ca Free TURN (limited but reliable)
     {
-        urls: 'turn:a.relay.metered.ca:80',
-        username: 'df4e050fc5de5dc26b25b85a',
-        credential: 'Pxdp2PK0b5ZXljOm'
-    },
-    {
-        urls: 'turn:a.relay.metered.ca:80?transport=tcp',
-        username: 'df4e050fc5de5dc26b25b85a',
-        credential: 'Pxdp2PK0b5ZXljOm'
-    },
-    {
-        urls: 'turn:a.relay.metered.ca:443',
-        username: 'df4e050fc5de5dc26b25b85a',
-        credential: 'Pxdp2PK0b5ZXljOm'
-    },
-    {
-        urls: 'turn:a.relay.metered.ca:443?transport=tcp',
+        urls: [
+            'turn:a.relay.metered.ca:80',
+            'turn:a.relay.metered.ca:80?transport=tcp',
+            'turn:a.relay.metered.ca:443',
+            'turns:a.relay.metered.ca:443?transport=tcp'
+        ],
         username: 'df4e050fc5de5dc26b25b85a',
         credential: 'Pxdp2PK0b5ZXljOm'
     },
 
-    // Additional free fallback STUN servers
+    // Additional Reliable STUN Servers
     { urls: 'stun:global.stun.twilio.com:3478' },
-    { urls: 'stun:stun.services.mozilla.com' },
-    { urls: 'stun:stun.ekiga.net' }
+    { urls: 'stun:stun.nextcloud.com:443' },
+    { urls: 'stun:stun.services.mozilla.com:3478' },
+    { urls: 'stun:stun.sipgate.net:3478' },
+    { urls: 'stun:stun.1und1.de:3478' },
+    { urls: 'stun:meet-jit-si-turnrelay.jitsi.net:443' }
 ];
 
 export const WEBRTC_CONFIG = {
