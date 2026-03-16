3ae633f6a61b3e0a9dcf2405c41adc1ed2c64d87
mishra-aashu <mishra-aashu@users.noreply.github.com>
2026-03-16 16:33:27 +0000

    deploy: b9c1960a3db15a198919c4e9ad51e0bfc5cbc693

---
+++ assets/useResolveName-CNvG1onV.js
@@ -0,0 +1,1 @@
+import{r as s}from"./vendor-react-0mK5w4BS.js";import{u as e}from"./dexie-react-hooks-CjuEhPZe.js";import{d as a}from"./AuthenticatedApp-BjUe3i57.js";const p=(t,n=null)=>{const r=e(()=>a.contacts.toArray())||[];return s.useMemo(()=>{if(!t)return n||"Unknown";const o=r?.find(c=>c.contact_user_id===t);return o&&o.contact_name?o.contact_name:n||"Unknown"},[t,n,r])};export{p as u};
